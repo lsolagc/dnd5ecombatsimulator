19c32c6f426c7ca2e0af566a59fa5df1dc9ca91c
Lucas Sola <lsolagc@hotmail.com>
2023-02-23 20:24:06 -0300

    feat: add default values to base combatants

--- db/migrate/20230222135409_create_base_combatants.rb
+++ db/migrate/20230222135409_create_base_combatants.rb
@@ -6,13 +6,13 @@ class CreateBaseCombatants < ActiveRecord::Migration[7.0]
       t.integer :hit_die
       t.integer :hit_dice_number
       t.integer :hitpoints
-      t.integer :proficiency_bonus
-      t.integer :strength
-      t.integer :dexterity
-      t.integer :constitution
-      t.integer :intelligence
-      t.integer :wisdom
-      t.integer :charisma
+      t.integer :proficiency_bonus, default: 2
+      t.integer :strength, default: 10
+      t.integer :dexterity, default: 10
+      t.integer :constitution, default: 10
+      t.integer :intelligence, default: 10
+      t.integer :wisdom, default: 10
+      t.integer :charisma, default: 10
       t.jsonb :skills
 
       t.timestamps
